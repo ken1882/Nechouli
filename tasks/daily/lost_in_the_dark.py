0495e85c2f032c2747b240144638aad8bc0652ba
Compeador <qj6w94g0q@gmail.com>
2025-12-05 21:12:16 +0800

    fix: bug

--- tasks/daily/lost_in_the_dark.py
+++ tasks/daily/lost_in_the_dark.py
@@ -8,16 +8,15 @@ class LostInTheDarkUI(BasePageUI):
         self.goto('https://www.neopets.com/games/lostinthedark/index.phtml')
         timer = datetime.now() + timedelta(minutes=5)
         while datetime.now() < timer:
-            btn = self.page.locator('input[value="Return to Haunted Woods"]')
-            if btn.count():
-                logger.info("GG")
-                break
-            btn = self.page.locator('input[value="Enter at Your Own Risk!"]')
-            if btn.count():
-                self.device.click(btn, nav=True)
-                continue
-            btn = self.page.locator('input[value="Right"]')
-            if btn.count():
+            btn = self.device.wait_for_element(
+                'input[value="Enter at Your Own Risk!"]',
+                'input[value="Right"]',
+                'input[value="Return to Haunted Woods"]',
+            )
+            if btn:
+                if 'return' in (btn.get_attribute('value') or '').lower():
+                    logger.info("GG")
+                    break
                 self.device.click(btn, nav=True)
                 continue
         return True
